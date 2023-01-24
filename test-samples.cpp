423cccc7c26aef295dd8e6dd585b7838a195efa2
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-01-24 17:25:00 +0530

    Update test-samples.cpp

--- test-samples.cpp
+++ test-samples.cpp
@@ -7,7 +7,7 @@ TEST_CASE("Test charging current samples") {
   int currentSample1[] = {4, 5};
   const int sampleCount1 = 1;
   char *sampleList1[sampleCount1];
-  int samples = CheckBatteryRange(currentSample1, &sampleList1[0]);
+  int samples = CheckBatteryRange(currentSample1, sampleList1);
   
   REQUIRE(samples == sampleCount1);
   REQUIRE(strncmp(sampleList1[0], "4-5, 2", strlen("4-5, 2")));
@@ -16,7 +16,7 @@ TEST_CASE("Test charging current samples") {
   int currentSample2[] = {5, 4};
   const int sampleCount2 = 1;
   char *sampleList2[sampleCount2];
-  int samples = CheckBatteryRange(currentSample2, &sampleList2[0]);
+  int samples = CheckBatteryRange(currentSample2, sampleList2);
 
   REQUIRE(samples == sampleCount2);
   REQUIRE(strncmp(sampleList2[0], "4-5, 2", strlen("4-5, 2")));   
