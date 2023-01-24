3b290ddf1b584407483e5568b709ca79edd053e6
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-01-24 15:39:08 +0530

    Create test-samples.cpp

---
+++ test-samples.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+
+
+TEST_CASE("Test charging current samples") {
+  
+}
