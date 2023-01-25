113b54242a473e7bca98bcd3f66abafc04f97971
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-01-25 06:22:55 +0530

    Update test-samples.cpp

--- test-samples.cpp
+++ test-samples.cpp
@@ -9,19 +9,27 @@ TEST_CASE("Test charge samples ranges") {
  
    formatOutputValue.formatOutput = formatOutput;
    int nrRange;
-   
+   printf("\nsample1-\nRange, Readings");
    int sample1[]= {4, 5}; 
    int n = sizeof(sample1)/sizeof(sample1[0]);  
    nrRange = DetectChargeRangeandCount(sample1, n);
    REQUIRE(nrRange == 1);
    REQUIRE(strncmp(str[0], "4-5, 2", strlen("4-5, 2")) == 0);
    
+   printf("\nsample2-\nRange, Readings");
    int sample2[] = {3, 3, 5, 4, 10, 11, 12};
    n = sizeof(sample2)/sizeof(sample2[0]); 
    nrRange = DetectChargeRangeandCount(sample2, n);
    REQUIRE(nrRange == 2);
    REQUIRE(strncmp(str[0], "3-5, 4", strlen("3-5, 4")) == 0);
    REQUIRE(strncmp(str[1], "10-12, 3", strlen("10-12, 3")) == 0);
+ 
+   printf("\nsample3-\nRange, Readings");
+   int sample1[]= {5,4}; 
+   int n = sizeof(sample3)/sizeof(sample3[0]);  
+   nrRange = DetectChargeRangeandCount(sample3, n);
+   REQUIRE(nrRange == 1);
+   REQUIRE(strncmp(str[0], "4-5, 2", strlen("4-5, 2")) == 0);
 }
 
 
