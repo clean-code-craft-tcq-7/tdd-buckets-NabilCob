3041bac959b34bf53119f0712fab19b0c4ee0840
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-01-25 06:09:08 +0530

    Update test-samples.cpp
    Test

--- test-samples.cpp
+++ test-samples.cpp
@@ -1,10 +1,18 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "test/catch.hpp"
-#include "FindRange.h"
 
-TEST_CASE("sample test") {
-  REQUIRE(add(2, 3) == 5);
+#include "FormatOutput.h"
+#include "DetectChargeRange.h"
+
+TEST_CASE("Test charge samples ranges") {
+   int arr[]= {4, 5}; 
+   int n = sizeof(arr)/sizeof(arr[0]);
+   formatOutputValue.formatOutput = formatOutput;
+   int nrRange;
+   nrRange = DetectChargeRangeandCount(arr, n);
+   REQUIRE(nrRange == 1);
+   REQUIRE(strncmp(str[0], "4-5, 2", strlen("4-5, 2")) == 0);
 }
 
 
