c9a66f7a970ffcbb35b11f8e2d785142bf432578
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-02-08 08:44:48 +0530

    Update test-samples.cpp

--- test-samples.cpp
+++ test-samples.cpp
@@ -33,6 +33,36 @@ TEST_CASE("Test charge samples ranges") {
    REQUIRE(nrRange == 2);
    REQUIRE(strncmp(str[0], "3-5, 4", strlen("3-5, 4")) == 0);
    REQUIRE(strncmp(str[1], "10-12, 3", strlen("10-12, 3")) == 0);
+ 
+   printf("\nsample3-\nRange, Readings");
+   int sample3[]= {1924, 1547}; 
+   n = sizeof(sample3)/sizeof(sample3[0]);  
+   int outputSample3[10];
+   config.bitConversion = 12;
+   config.maxAmps = 10;
+   size = ConvertAnalogSamplestoDigital(sample3, n, config, outputSample3);
+   nrRange = DetectChargeRangeandCount(outputSample3, size);
+   REQUIRE(nrRange == 1);
+   REQUIRE(strncmp(str[0], "4-5, 2", strlen("4-5, 2")) == 0);
+ 
+   printf("\nsample4-\nRange, Readings");
+   int sample4[]= {1146, 1924, 2702};
+   n = sizeof(sample4)/sizeof(sample4[0]);
+   int outputSample4[10];
+   config.bitConversion = 12;
+   config.maxAmps = 10;
+   size = ConvertAnalogSamplestoDigital(sample4, n, config, outputSample4);
+   nrRange = DetectChargeRangeandCount(outputSample4, size);
+   REQUIRE(nrRange == 3);
+   REQUIRE(strncmp(str[0], "3-3, 1", strlen("3-3, 1")) == 0);
+   REQUIRE(strncmp(str[1], "5-5, 1", strlen("5-5, 1")) == 0);
+   REQUIRE(strncmp(str[2], "7-7, 1", strlen("7-7, 1")) == 0);
+ 
+   printf("\nsample5-\nEmpty sample");
+   size = ConvertAnalogSamplestoDigital(NULL, 0, 0, NULL);
+   REQUIRE(size == 0);
+   nrRange = DetectChargeRangeandCount(NULL, 0);
+   REQUIRE(nrRange == 0);
 }
 
 
