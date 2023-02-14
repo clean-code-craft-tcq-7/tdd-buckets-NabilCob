40f75a710dc924538312ee64a262a8bbb90967c1
NabilCob <69570628+NabilCob@users.noreply.github.com>
2023-02-14 14:15:49 +0530

    Update A2DConversion.c

--- A2DConversion.c
+++ A2DConversion.c
@@ -8,13 +8,15 @@ int ConvertAnalogSamplestoDigital(int inputsample[], int sampleSize,
    int sampleCnt = 0;
    int outputCnt = 0;
    int range = 0;
+   int value = 0;
    for (sampleCnt = 0; sampleCnt < sampleSize; sampleCnt ++){
 
        if(checklimits(inputsample[sampleCnt], config.bitConversion)){
            //Calculate range
            range = config.maxAmps - config.minAmps;
-           outputsample[outputCnt]= mapOperatingRange(A2DConvertion(inputsample[sampleCnt],range,
+           value = mapOperatingRange(A2DConvertion(inputsample[sampleCnt],range,
                            config.bitConversion), config.minAmps);
+           outputsample[outputCnt] = abs(value);
            outputCnt ++;
        }
    }
